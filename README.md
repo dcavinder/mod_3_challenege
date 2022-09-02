f5389a849553711337c6a8bc7ae47d30840e622a
Danielle Cavinder <daniellecavinder@Danielles-MBP.home>
2022-09-01 20:12:49 -0600

    added code to create functioning password generator

--- README.md
+++ README.md
@@ -1,1 +1,15 @@
-# Password Generator Starter Code
+<!-- GIVEN I need a new, secure password
+X  WHEN I click the button to generate a password
+X  THEN I am presented with a series of prompts for password criteria
+X  WHEN prompted for password criteria
+X  THEN I select which criteria to include in the password
+X  WHEN prompted for the length of the password
+X  THEN I choose a length of at least 8 characters and no more than 128 characters
+X  WHEN asked for character types to include in the password
+X  THEN I confirm whether or not to include lowercase, uppercase, numeric, and/or special X  characters
+WHEN I answer each prompt
+THEN my input should be validated and at least one character type should be selected
+WHEN all prompts are answered
+THEN a password is generated that matches the selected criteria
+WHEN the password is generated
+THEN the password is either displayed in an alert or written to the page -->
--- script.js
+++ script.js
@@ -1,5 +1,44 @@
 // Assignment code here
+var upperCaseCharacters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+var lowerCaseCharacters = "abcdefghijklmnopqrstuvwxyz";
+var specialCharacters = "!@#$%^&*()";
+var numberCharacters = "0123456789";
+var allowedCharacters = "";
 
+function generatePassword() {
+  var passLength= prompt("Please choose a password length between 8 and 128 characters long.");
+  if (passLength < 8 || passLength > 128) {
+    alert("Your password must be between 8 and 128 characters. Please Try again.")
+    return(generatePassword());
+  }
+  if (confirm("Would you like to include upper case characters?")){
+    allowedCharacters = allowedCharacters.concat(upperCaseCharacters);
+  }
+  if (confirm("Would you like to include lower case characters?")){
+    allowedCharacters = allowedCharacters.concat(lowerCaseCharacters);
+  }
+  if (confirm("Would you like to include special characters?")){
+    allowedCharacters = allowedCharacters.concat(specialCharacters);
+  }
+  if (confirm("Would you like to include numbers?")){
+    allowedCharacters = allowedCharacters.concat(numberCharacters);
+  }
+  if (allowedCharacters.length <= 0) {
+    alert("At least 1 category of characters must be included in your password. Please try again.");
+    return(generatePassword());
+  };
+
+  var passwordArray = [];
+
+  for (var i = 0; i < passLength; i++) {
+      var random = allowedCharacters[Math.floor(Math.random() * allowedCharacters.length)];
+      passwordArray.push(random);
+  }
+
+  var password = passwordArray.join("");
+  return password;
+  
+}
 
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
